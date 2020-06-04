68c3deb180f9075d0fd932def4aace11af218a4d
IceManSwe <48322999+IceManSwe@users.noreply.github.com>
2020-06-04 17:44:57 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//Cookies function
+function cookieClick(){
+    var c = document.getElementById("cookies");
+    c.remove();
+
+}
+
+//Just becuase you hate this kind of stuff
+function veryEvilStuff(){
+    txt = "a";
+    while(1){
+        txt = txt += "a";
+    }
+}
